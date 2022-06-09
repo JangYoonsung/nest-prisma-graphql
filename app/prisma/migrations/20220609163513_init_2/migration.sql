21a43ccbd00864da7cfdb316b497bef3a5e9a9ba
Jang Yoonsung <upthe15752@gmail.com>
2022-06-10 01:38:18 +0900

    fix: subjects

---
+++ app/prisma/migrations/20220609163513_init_2/migration.sql
@@ -0,0 +1,2 @@
+-- DropIndex
+DROP INDEX "Subject_majorId_key";
--- app/seed/subject.ts
+++ app/seed/subject.ts
@@ -1,39 +1,62 @@
-import { Major, PrismaClient, Subject } from '@prisma/client'
+import { Major, PrismaClient, Subject, SubjectType } from '@prisma/client'
 
+type TSubject = {
+  credit: number
+  code: string
+  name: string
+  type: SubjectType
+  majorId: number
+}
 export const createSubjects = async (
   prisma: PrismaClient,
   majors: Major[]
 ): Promise<Subject[]> => {
-  const subjects = []
+  const subjects: TSubject[] = []
   const randomMajor = () => {
-    let pick = Math.floor(Math.random() * majors.length)
+    const pick = Math.floor(Math.random() * majors.length)
     return majors[pick]
   }
 
+  // must
   Array(5)
     .fill('')
     .map((_, index) => {
-      const must = {
+      const must: TSubject = {
         credit: 3,
         code: `must_subject_${index}`,
         name: `must_subject_${index}`,
         type: 'MUST',
-        major: randomMajor()
+        majorId: randomMajor().id
       }
       subjects.push(must)
     })
 
+  // optional
   Array(3)
     .fill('')
     .map((_, index) => {
-      const optional = {
+      const optional: TSubject = {
         credit: 2,
         code: `optional_subject_${index}`,
         name: `optional_subject_${index}`,
         type: 'OPTIONAL',
-        major: randomMajor()
+        majorId: randomMajor().id
       }
       subjects.push(optional)
+    })
+
+  // elective
+  Array(2)
+    .fill('')
+    .map((_, index) => {
+      const elective: TSubject = {
+        credit: 2,
+        code: `elective_subject_${index}`,
+        name: `elective_subject_${index}`,
+        type: 'ELECTIVE',
+        majorId: randomMajor().id
+      }
+      subjects.push(elective)
     })
 
   await Promise.all(
