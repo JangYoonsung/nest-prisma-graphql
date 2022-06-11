5557e5f101c08ed23498223799b5f0f860cced2b
Jang Yoonsung <upthe15752@gmail.com>
2022-06-12 00:57:24 +0900

    fix: update and create dto

--- app/src/major/dto/update-major.dto.ts
+++ app/src/major/dto/update-major.dto.ts
@@ -1,7 +1,9 @@
 import { Field, InputType } from '@nestjs/graphql'
+import { IsOptional } from 'class-validator'
 
 @InputType()
 export class UpdateMajorDto {
   @Field(() => String, { nullable: true })
+  @IsOptional()
   name?: string
 }
