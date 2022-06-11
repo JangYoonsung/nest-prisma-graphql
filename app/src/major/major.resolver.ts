443306009fc9b5cfa9ced31fd07a6b4799c2c3a3
Jang Yoonsung <upthe15752@gmail.com>
2022-06-12 01:26:59 +0900

    refactor: major

--- app/src/major/major.resolver.ts
+++ app/src/major/major.resolver.ts
@@ -1,28 +1,30 @@
 import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql'
-import { PrismaService } from 'src/config/prisma.service'
 import { CreateMajorDto } from './dto/create-major.dto'
 import { UpdateMajorDto } from './dto/update-major.dto'
+import { MajorService } from './major.service'
 import { Major } from './model/major.model'
 
 @Resolver(() => Major)
 export class MajorResolver {
-  constructor(private prisma: PrismaService) {}
+  constructor(private majorService: MajorService) {}
 
   @Query(() => [Major])
-  async getMajors() {
-    return this.prisma.major.findMany()
+  async getMajors(
+    @Args('currentPage', { type: () => Int, nullable: true }) currentId?: number
+  ) {
+    return this.majorService.getMajors(currentId)
   }
 
   @Query(() => Major)
   async getMajor(@Args('id', { type: () => Int }) id: number): Promise<Major> {
-    return this.prisma.major.findUnique({ where: { id } })
+    return this.majorService.getMajor(id)
   }
 
   @Mutation(() => Major)
   async createMajor(
     @Args('dto', { type: () => CreateMajorDto }) dto: CreateMajorDto
   ): Promise<Major> {
-    return this.prisma.major.create({ data: dto })
+    return this.majorService.createMajor(dto)
   }
 
   @Mutation(() => Major)
@@ -30,9 +32,11 @@ export class MajorResolver {
     @Args('id', { type: () => Int }) id: number,
     @Args('dto', { type: () => UpdateMajorDto }) dto: UpdateMajorDto
   ): Promise<Major> {
-    return this.prisma.major.update({
-      where: { id },
-      data: dto
-    })
+    return this.majorService.updateMajor(id, dto)
+  }
+
+  @Mutation(() => Major)
+  async deleteMajor(@Args('id', { type: () => Int }) id: number) {
+    return this.majorService.deleteMajor(id)
   }
 }
